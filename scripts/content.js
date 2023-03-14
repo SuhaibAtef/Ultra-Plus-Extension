a50fd9525ec88cd137460dc6391848c06cbe58d1
Suhaib Atef <Suhaibatef20@gmail.com>
2023-03-14 20:25:13 +0300

    Initial Idea and Files

---
+++ scripts/content.js
@@ -0,0 +1,122 @@
+var coursesResources = {
+    CPE200: "https://drive.google.com/drive/folders/1pRM4p9EGjUG-h1igE5lgHHZHBH_U-E6g?usp=share_link",
+    CPE211: "https://drive.google.com/drive/folders/1mgD1P8zS6fRS-rn6z0IodTiw4My5wBAV?usp=share_link",
+    CPE231: "https://drive.google.com/drive/folders/1xXFk1Z3VPWRkPkXly3Kp5hFvfq5LJ4jp?usp=share_link",
+    CPE232: "https://drive.google.com/drive/folders/1SHb9v_bUpBbC8OXRd_Fkh1P7V4GG6muY?usp=share_link",
+    CPE252: "https://drive.google.com/drive/folders/1p6DhZQLMjhE2wnNAHDQjpElWOiEaDZh7?usp=share_link",
+    CPE300: "https://drive.google.com/drive/folders/17RdsyrX1P9ZXdJ4CwlzfshM5uiMP1nSi?usp=share_link",
+    CPE311: "https://drive.google.com/drive/folders/1aqO0Aur8eUDshTP2hTnsDv8i1FlfkO-Q?usp=share_link",
+    CPE351: "https://drive.google.com/drive/folders/1undm5Th9V6aJxCAlAt55NgN8yVSvJi31?usp=share_link",
+    CPE352: "https://drive.google.com/drive/folders/1jK_cd8Pb59jXOsCjVrGb3A87vDKMZkDp?usp=share_link",
+    CPE354: "https://drive.google.com/drive/folders/1dqp8vVnGv3ZocPhNDT6EgN1-YFumvKuc?usp=share_link",
+    CPE421: "https://drive.google.com/drive/folders/1gpm7RFRYqo1EYQkVjVPAH6KGBIN3vn50?usp=share_link",
+    CPE451: "https://drive.google.com/drive/folders/1H_DkBJFchXOfa-8STaX4J8KeGhGh0iOe?usp=share_link",
+    CPE454: "https://drive.google.com/drive/folders/1HbZunfAe8KQlaU5Lofa0AiBhh2e48yJT?usp=share_link",
+    CPE473: "https://drive.google.com/drive/folders/111TCauvxzKr5UKlVAlkD3wqXRlxyA1K4?usp=share_link",
+    CPE480: "https://drive.google.com/drive/folders/16QwFuAoSnc0YQRfoAsrSOZG8aUV0aaKg?usp=share_link",
+    CPE481: "https://drive.google.com/drive/folders/16lnzI3Do6l33-Tz6uw7GCcSSVcUI4Ra_?usp=share_link",
+    CPE491: "https://drive.google.com/drive/folders/1c_eXMhubWiYkFjs4c_3ZQrJZyi2-peqH?usp=share_link",
+    CPE510: "https://drive.google.com/drive/folders/1YNpBdqSyFAwp1x3-CHVACwd6AcMvdESQ?usp=share_link",
+    CPE591: "https://drive.google.com/drive/folders/1_yyfcnbyL24LERQTNEwkeGixcx38B17x?usp=share_link",
+    CPE592: "https://drive.google.com/drive/folders/13-u7q9E4ACd9lDx7cr1glgzHcFtNN9EC?usp=share_link",
+};
+
+var coursesPosts = {
+    CPE200: "https://drive.google.com/drive/folders/1pRM4p9EGjUG-h1igE5lgHHZHBH_U-E6g?usp=share_link",
+    CPE211: "https://drive.google.com/drive/folders/1mgD1P8zS6fRS-rn6z0IodTiw4My5wBAV?usp=share_link",
+    CPE231: "https://drive.google.com/drive/folders/1xXFk1Z3VPWRkPkXly3Kp5hFvfq5LJ4jp?usp=share_link",
+    CPE232: "https://drive.google.com/drive/folders/1SHb9v_bUpBbC8OXRd_Fkh1P7V4GG6muY?usp=share_link",
+    CPE252: "https://drive.google.com/drive/folders/1p6DhZQLMjhE2wnNAHDQjpElWOiEaDZh7?usp=share_link",
+    CPE300: "https://drive.google.com/drive/folders/17RdsyrX1P9ZXdJ4CwlzfshM5uiMP1nSi?usp=share_link",
+    CPE311: "https://drive.google.com/drive/folders/1aqO0Aur8eUDshTP2hTnsDv8i1FlfkO-Q?usp=share_link",
+    CPE351: "https://drive.google.com/drive/folders/1undm5Th9V6aJxCAlAt55NgN8yVSvJi31?usp=share_link",
+    CPE352: "https://drive.google.com/drive/folders/1jK_cd8Pb59jXOsCjVrGb3A87vDKMZkDp?usp=share_link",
+    CPE354: "https://drive.google.com/drive/folders/1dqp8vVnGv3ZocPhNDT6EgN1-YFumvKuc?usp=share_link",
+    CPE421: "https://drive.google.com/drive/folders/1gpm7RFRYqo1EYQkVjVPAH6KGBIN3vn50?usp=share_link",
+    CPE451: "https://drive.google.com/drive/folders/1H_DkBJFchXOfa-8STaX4J8KeGhGh0iOe?usp=share_link",
+    CPE454: "https://drive.google.com/drive/folders/1HbZunfAe8KQlaU5Lofa0AiBhh2e48yJT?usp=share_link",
+    CPE473: "https://drive.google.com/drive/folders/111TCauvxzKr5UKlVAlkD3wqXRlxyA1K4?usp=share_link",
+    CPE480: "https://drive.google.com/drive/folders/16QwFuAoSnc0YQRfoAsrSOZG8aUV0aaKg?usp=share_link",
+    CPE481: "https://drive.google.com/drive/folders/16lnzI3Do6l33-Tz6uw7GCcSSVcUI4Ra_?usp=share_link",
+    CPE491: "https://drive.google.com/drive/folders/1c_eXMhubWiYkFjs4c_3ZQrJZyi2-peqH?usp=share_link",
+    CPE510: "https://drive.google.com/drive/folders/1YNpBdqSyFAwp1x3-CHVACwd6AcMvdESQ?usp=share_link",
+    CPE591: "https://drive.google.com/drive/folders/1_yyfcnbyL24LERQTNEwkeGixcx38B17x?usp=share_link",
+    CPE592: "https://drive.google.com/drive/folders/13-u7q9E4ACd9lDx7cr1glgzHcFtNN9EC?usp=share_link",
+};
+
+window.addEventListener("load", myMain, false);
+
+function myMain(evt) {
+    // darkMode();
+    addLinks();
+
+}
+
+function darkMode() {
+    document.body.style.backgroundColor = "#000000"; // Background Color
+    document.querySelector(".main-inner").style.backgroundColor = "#000000"; // main inner color
+
+    // Text Color is white
+    var allTextElements = document.getElementsByTagName("*");
+    for (var i = 0; i < allTextElements.length; i++) {
+        allTextElements[i].style.color = "#ffffff";
+    }
+
+    // Navigation Bar
+    document.querySelector(".navbar-brand").innerText = "Ultra Bytes"; // Title 
+    document.querySelector(".moremenu .nav-link.active").style.borderBottomColor = "#FF0000"; // Active tab border color
+    document.querySelector(".navbar").style.borderBottom = "#FF0000 2px solid"; // Line 
+    document.querySelector(".navbar").style.boxShadow = "0 0 10px #FF0000"; // Glow
+
+    // Links at top
+    collection = document.querySelectorAll(".navbar-light .navigation .nav-link");
+    for (var i = 0; i < collection.length; i++) {
+        collection[i].style.color = "#FF0000";
+    }
+    //icons
+    collection = document.querySelectorAll(".icon");
+    for (var i = 0; i < collection.length; i++) {
+        collection[i].style.color = "#FF0000";
+    }
+}
+
+
+function addLinks() {
+    var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
+
+    function checkForJS_Finish() {
+        if (typeof SOME_GLOBAL_VAR != "undefined"
+            || document.querySelector("#page-container-1 > div > div")
+        ) {
+            clearInterval(jsInitChecktimer);
+            var list = document.querySelector('#page-container-1 > div > div'); // List of cards 
+            const listItems = list.children;
+            for (var i = 0; i < listItems.length; i++) {
+                // console.log(list[i].id);
+                var item = listItems[i];
+                // aalink coursename mr-2 mb-1  / #course-info-container-190-3 > div > div > a 
+                title = item.querySelector("a.aalink span.multiline").textContent.replace(/\s/g, '').slice(0, 6);
+                if (coursesResources.hasOwnProperty(title)) {
+                    // console.log(dict[title]); // aquire link for the element.
+                    var btn = document.createElement("a");
+                    btn.innerHTML = "Ultra Bytes Resources";
+                    btn.classList.add("btn", "btn-primary", "btn-sm", "rounded-0");
+                    btn.href = coursesResources[title];
+                    btn.target = "_blank";
+                    btn.style.width = "100%";
+                    var btn2 = document.createElement("a");
+                    btn2.innerHTML = "البوست الشامل";
+                    btn2.classList.add("btn", "btn-danger", "btn-sm", "rounded-0");
+                    btn2.href = coursesResources[title];
+                    btn2.style.width = "100%";
+                    btn2.target = "_blank";
+                    var diva = document.createElement("div");
+                    diva.appendChild(btn);
+                    diva.appendChild(btn2);
+                    item.appendChild(diva);
+                }
+
+            }
+        }
+    }
+}
